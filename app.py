22fdddc08c964221dfab4b18a697a08cdd6ed43d
Kosta Dunn <kostacoffee@gmail.com>
2017-08-20 00:06:10 +1000

    API changed to 'spiders'

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 import requests
 import base64
 import api_auth
+import json
 
 app = Flask(__name__)
 
@@ -22,7 +23,7 @@ def spider():
     auth = (api_auth.USER, api_auth.PASS)
     b64img = base64.b64encode(img).decode()
 
-    data = '{"service":"%s","image":"%s"}'%('tagging2',b64img)
+    data = '{"service":"%s","image":"%s"}'%('spider',b64img)
 
     uploaded_task = requests.post(
         'http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
@@ -51,6 +52,54 @@ def spider():
 
     return 'description: {}, confidence: {}'.format(desc, confidence)
 
+@app.route('/identifyMob', methods=['POST'])
+def spiderMob():
+
+    if 'upload' in request.files:
+        b64img = base64.b64encode(request.files['upload'].read()).decode()
+    elif 'imageData' in request.form:
+        b64img = request.form['imageData']
+
+    json_header = {'Content-Type': 'application/json'}
+
+    auth = (api_auth.USER, api_auth.PASS)
+
+    data = '{"service":"%s","image":"%s"}'%('spiders',b64img)
+
+    uploaded_task = requests.post(
+        'http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
+        data = data,
+        auth = auth,
+        headers = json_header
+    )
+
+    resp = uploaded_task.json()
+    print(resp)
+
+    id = resp['task']['uri'].split('/')[-1]
+
+    scan_task = requests.put(
+        'http://smartvision.aiam-dh.com:8080/api/v1.0/tasks/run/{}'.format(id),
+        data = '{"scanned": true}',
+        auth = auth,
+        headers = json_header
+    )
+
+    print(scan_task.text)
+    resp = scan_task.json()['task']
+
+    desc = resp['description']
+    confidence = float(resp['confidence'])
+
+    if confidence < 60:
+        return json.dumps({
+            "name": "try_again"
+        })
+
+    return json.dumps({
+        "name": desc
+    })
+
 @app.route('/result')
 def dummy() :
     info = {
@@ -63,4 +112,4 @@ def dummy() :
     return render_template("result.html", info=info)
 
 
-app.run(debug=True, threaded=True)
+app.run(host='0.0.0.0', debug=True, threaded=True)
