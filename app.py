71b5c5936eb1f45da559805a060aa49065f58dfb
Kosta Dunn <kostacoffee@gmail.com>
2017-08-19 14:41:54 +1000

    Added index route and fixed templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return 'this is the root page with the upload photo button'
+    print("HELLO")
+    return render_template('upload.html')
 
 @app.route('/identify', methods=['POST'])
 def spider():
@@ -16,6 +17,7 @@ def spider():
 
     # TODO send image to api
 
-    return 'spider classification page'
+    return 'TODO result.html'
+
 
 app.run()
