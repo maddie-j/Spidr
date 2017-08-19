47abcff148fc1d6c3f5c6787e1c236cc41e1ad0d
Kosta Dunn <kostacoffee@gmail.com>
2017-08-19 15:33:39 +1000

    Added accenture api calls

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, redirect, render_template
+import requests
+import base64
+import api_auth
 
 app = Flask(__name__)
 
@@ -13,11 +16,38 @@ def spider():
     if 'image' not in request.files:
         return redirect(request.url)
 
-    img = request.files['image']
-
-    # TODO send image to api
-
-    return 'TODO result.html'
+    img = request.files['image'].read()
 
 
-app.run()
+    auth = (api_auth.USER, api_auth.PASS)
+    uploaded_task = requests.post(
+        'http://smartvision.aiam-dh.com:8080/api/v1.0/tasks',
+        data = {
+            'service': 'tagging1',
+            'image': base64.b64encode(img)
+        },
+        auth = auth
+    )
+
+    resp = uploaded_task.json()
+
+    print("IMAGE ID:{}".format(resp['id']))
+
+    scan_task = requests.put(
+        'http://smartvision.aiam-dh.com:8080/api/v1.0/tasks/run/{}'.format(resp['id']),
+        data = {
+            "scanned": True
+        },
+        auth = auth
+    )
+
+    resp = scan_task.json()
+
+    desc = resp['description'].split(',')
+    print(resp)
+    confidence = [int(a) for a in resp['confidence'].split(',')]
+
+    return 'description: {}, confidence: {}'.format(desc, confidence)
+
+
+app.run(debug=True)
